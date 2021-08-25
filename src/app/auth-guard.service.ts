28f172dd0643c19dc7bcf7e227731446c4daa2d2
Luc Cornelissen <l.cornelissen@global.leaseweb.com>
2021-08-25 22:16:04 +0200

    add redirect after login

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
 import { CanActivate } from '@angular/router';
-import { Router } from '@angular/router';
+import { Router, RouterStateSnapshot } from '@angular/router';
 import 'rxjs/add/operator/map';
 
 
@@ -12,11 +12,11 @@ export class AuthGuard implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate() {
+  canActivate(route: any, state: RouterStateSnapshot) {
     return this.auth.user$.map(user => {
       if (user) return true;
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
       return false;
     });
   }
