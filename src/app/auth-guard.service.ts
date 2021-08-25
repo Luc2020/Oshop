1c20d1da2e423811e41951862935e9fcfa39a148
Luc Cornelissen <l.cornelissen@global.leaseweb.com>
2021-08-25 16:20:04 +0200

    add Auth guard service

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+import { CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/map';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate() {
+    return this.auth.user$.map(user => {
+      if (user) return true;
+
+      this.router.navigate(['/login']);
+      return false;
+    });
+  }
+
+}
