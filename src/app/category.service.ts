46b1639f238b32dde59d9732c57e3cfe520c2f16
Luc Cornelissen <l.cornelissen@global.leaseweb.com>
2021-08-30 17:47:14 +0200

    categories filter with products (not filterin)

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,19 @@ export class CategoryService {
   constructor(private db: AngularFireDatabase) { }
 
   getAll() {
-    return this.db.list('/categories', ref =>
-    ref.orderByChild('name')).snapshotChanges() as Observable<any>;
-  }
+    return this.db.list('/categories').snapshotChanges()
+    .pipe(map( action => action
+      .map(a => {
+        const key = a.payload.key;
+        const data = a.payload.val();
+          return  {key, data};
+      })));
+   }
+
+
+  // getAll() {
+  //   return this.db.list('/categories', ref =>
+  //   ref.orderByChild('name')).snapshotChanges() as Observable<any>;
+  // }
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import { ProductService } from 'src/app/product.service';
 import { CategoryService } from 'src/app/category.service';
+import { ActivatedRoute } from '@angular/router'
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/take';
 
@@ -12,10 +13,18 @@ import 'rxjs/add/operator/take';
 export class ProductsComponent {
   products$: Observable<any>;
   categories$: any;
+  category!: string | null;
 
-  constructor(private productService: ProductService, private categoryService: CategoryService,) {
+  constructor(
+    route: ActivatedRoute,
+    private productService: ProductService,
+    private categoryService: CategoryService,) {
     this.products$ = this.productService.getAll();
     this.categories$ = this.categoryService.getAll();
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+    })
   }
 
 
